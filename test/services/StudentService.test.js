d5eb095a1b4b11d049f4eb123064e6fb5eade039
Sheepshenko <sheepshenko@gmail.com>
2022-05-08 18:53:01 -0500

    Test to validate that the credits of the students are greater than 500.

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -8,8 +8,14 @@ describe("Test of the StudentService",() => {
     });
 
     test("2. Validate if the student has the Certification", () => {
-        const students = [{"haveCertification": true}];
+        const students = [{"haveCertification": true,"email": "test@mail.com"}];
         const validateCertification = StudentService.getEmailStudentsCertifed(students);
-        expect(validateCertification.length).toBe(1);
+        expect(validateCertification).toContain("test@mail.com");
     });
+
+    test("3. Validate that the student has more than 500 credits", () =>{
+        const students = [{"credits": 545}];
+        const validateCredits = StudentService.getStudentsCredits(students);
+        expect(validateCredits.length).toBe(1);
+    })
 });
